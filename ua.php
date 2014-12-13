3c8239fba063e18dc7d250ce61baa0868640eeb4
Gomasy <nyan@gomasy.jp>
2014-12-14 02:51:29 +0900

    Update ua.php

--- ua.php
+++ ua.php
@@ -2,9 +2,9 @@
 
 function load_css() {
     $ua = $_SERVER['HTTP_USER_AGENT'];
-    if (preg_match('/MSIE\s[4-9]/', $ua) || ($_GET['p'] === 'ie')) {
+    if (preg_match('/MSIE\s[4-9]/', $ua) || ((isset($_GET['p'])) && ($_GET['p'] === 'ie'))) {
         return '<link rel="stylesheet" href="./css/style-ie.css">'."\n";
-    } else if (preg_match('/Android\s2.[1-3]/', $ua) || ($_GET['p'] === 'min')) {
+    } else if (preg_match('/Android\s2.[1-3]/', $ua) || ((isset($_GET['p'])) && ($_GET['p'] === 'min'))) {
         return '<link rel="stylesheet" href="./css/style-min.css">'."\n";
     } else {
         return '<link rel="stylesheet" href="./css/style.css">'."\n";
