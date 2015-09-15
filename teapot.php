2c26837e6d7d12972c9a334e7a8d0ae40e507891
Gomasy <nyan@gomasy.jp>
2015-09-15 21:27:26 +0900

    Add teapot.php

---
+++ teapot.php
@@ -0,0 +1,19 @@
+<?php
+  header("HTTP/1.1 418 I'm a teapot");
+  $ua = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+?>
+<html>
+<head><title>418 I'm a teapot</title></head>
+<body bgcolor="white">
+<center><h1>418 I'm a teapot</h1></center>
+<hr><center>nginx</center>
+</body>
+</html>
+<?php if ((preg_match('/MSIE\s.+?/', $ua)) || (preg_match('/Chrome\/.+?/', $ua))) { ?>
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<!-- a padding to disable MSIE and Chrome friendly error page -->
+<?php } ?>
