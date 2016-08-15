2a796eb308c78bb9cf8cc43525ea16a43b29fa3e
Gomasy <nyan@gomasy.jp>
2016-08-16 00:53:02 +0900

    Update likes

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
         <div class="modal-window" id="about">
           <div class="modal-text">
             <p>I can't do anything.</p>
-            <p>Like: Arch Linux, C#, Ruby, Ingress<br>
+            <p>Like: Arch Linux, Ruby, Ingress<br>
             <p>MAIL: <span id="mail-text">$ echo 'bnlhbkBnb21hc3kuanA=' | base64 -d</span></p>
           </div>
           <a class="modal-close" href="#">Close</a>
