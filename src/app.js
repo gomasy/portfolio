d37716a19daea06b2cc626f6770d1835ed3291c4
Gomasy <nyan@gomasy.jp>
2017-10-11 11:33:44 +0900

    Update google analytics codes

--- src/app.js
+++ src/app.js
@@ -1,10 +1,16 @@
 require('font-awesome/scss/font-awesome.scss');
 require('./style.scss');
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+/* Global site tag (gtag.js) - Google Analytics */
+var ga = document.createElement('script');
+ga.async = true;
+ga.src = '//www.googletagmanager.com/gtag/js?id=UA-54951000-1';
 
-ga('create', 'UA-54951000-1', 'auto');
-ga('send', 'pageview');
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(ga, s);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+
+gtag('js', new Date());
+gtag('config', 'UA-54951000-1');
