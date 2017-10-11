b1284eb8c6d08e7bd064cc265ebc53beff749fa8
Gomasy <nyan@gomasy.jp>
2017-10-11 15:52:54 +0900

    Fix redundant codes

--- src/app.js
+++ src/app.js
@@ -2,9 +2,10 @@ require('font-awesome/scss/font-awesome.scss');
 require('./style.scss');
 
 /* Global site tag (gtag.js) - Google Analytics */
+var trackingId = 'UA-54951000-1';
 var ga = document.createElement('script');
 ga.async = true;
-ga.src = '//www.googletagmanager.com/gtag/js?id=UA-54951000-1';
+ga.src = '//www.googletagmanager.com/gtag/js?id=' + trackingId;
 
 var s = document.getElementsByTagName('script')[0];
 s.parentNode.insertBefore(ga, s);
@@ -13,4 +14,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
 
 gtag('js', new Date());
-gtag('config', 'UA-54951000-1');
+gtag('config', trackingId);
