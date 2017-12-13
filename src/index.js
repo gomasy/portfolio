e46a3585218da0a0272e4ef4f52fb718780ecf15
Gomasy <nyan@gomasy.jp>
2017-12-13 12:24:44 +0900

    Add viewport element

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import App from './components/app';
 import './contents/style.scss';
 import './contents/favicon.ico';
 
+const metaNode = document.createElement('meta');
+metaNode.name = 'viewport';
+metaNode.content = 'width=device-width, initial-scale=1';
+document.head.appendChild(metaNode);
+
 const appNode = document.createElement('div');
 appNode.id = 'app';
 document.body.appendChild(appNode);
