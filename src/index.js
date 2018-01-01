80b6869eeedc1e44021710a2807017bde9fb42c1
Gomasy <nyan@gomasy.jp>
2018-01-01 20:12:07 +0900

    Add viewport element

--- src/index.js
+++ src/index.js
@@ -6,6 +6,11 @@ import './gtag';
 import './contents/style.scss';
 import './contents/favicon.ico';
 
+const meta = document.createElement('meta');
+meta.name = 'viewport';
+meta.content = 'width=device-width, initial-scale=1';
+document.head.appendChild(meta);
+
 const root = document.createElement('div');
 root.id = 'main';
 document.body.appendChild(root);
