f99133f6af320af7361850af7b6420c4d987d4d1
Gomasy <nyan@gomasy.jp>
2018-05-19 12:10:16 +0900

    Update styles

--- src/app.js
+++ src/app.js
@@ -1,47 +1,47 @@
 import { h } from 'hyperapp';
 
 const Profile = ({ icon, name }) => (
-    <div id="profile">
+    <div class="profile">
         <img src={icon} />
         <span>{name}</span>
     </div>
 );
 
 const Buttons = ({ openModal }) => (
-    <div id="buttons">
-        <a id="about" onclick={openModal}>About</a>
-        <a id="blog" href="https://blog.gomasy.jp/">Blog</a>
-        <a id="github" href="https://github.com/Gomasy">GitHub</a>
-        <a id="twitter" href="https://twitter.com/u597">Twitter</a>
-        <a id="skype" href="skype:ktomohiro365">Skype</a>
+    <div class="buttons">
+        <a class="about" onclick={openModal}>About</a>
+        <a class="blog" href="https://blog.gomasy.jp/">Blog</a>
+        <a class="github" href="https://github.com/Gomasy">GitHub</a>
+        <a class="twitter" href="https://twitter.com/u597">Twitter</a>
+        <a class="skype" href="skype:ktomohiro365">Skype</a>
     </div>
 );
 
 const ModalWindow = ({ state, closeModal }) => (
-    <div class={state} id="modal-window">
-        <div id="modal-text">
+    <div class="modal-window" id={state}>
+        <div class="modal-text">
             <p>なにもできません。<br />
             Like: Arch Linux, Ruby, PHP, Vim</p>
             <p>Mail: <span class="hl">$ echo 'bnlhbkBnb21hc3kuanA=' | base64 -d</span><br />
             PGP Key: <span class="hl">02BE 7F39 D978 AC7E</span></p>
         </div>
-        <a id="modal-close" onclick={closeModal}>[Close]</a>
+        <a class="modal-close" onclick={closeModal}>[Close]</a>
     </div>
 );
 
 export const state = {
     icon: require('./contents/icon.jpg'),
     name: 'Gomasy',
-    modal: 'hide',
+    modal: '',
 };
 
 export const actions = {
     open: () => state => ({ modal: 'show' }),
-    close: () => state => ({ modal: 'hide' }),
+    close: () => state => ({ modal: '' }),
 };
 
 export const view = (state, actions) => (
-    <div id="container">
+    <div class="container">
         <Profile icon={state.icon} name={state.name} />
         <Buttons openModal={actions.open} />
         <ModalWindow state={state.modal} closeModal={actions.close} />
