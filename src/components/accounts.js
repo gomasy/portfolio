1ad492dd6c470e7f98656d0057e0d8cf20fb956a
Gomasy <nyan@gomasy.jp>
2020-03-05 18:06:44 +0900

    Update accounts

--- src/components/accounts.js
+++ src/components/accounts.js
@@ -5,11 +5,12 @@ export const Accounts = () =>
     <div class="accounts">
         <Header text="Accounts" />
         <div class="accounts-container">
-            <Account icon="&#xf781;" text="Blog" href="https://blog.gomasy.jp/" />
             <Account icon="&#xf09b;" text="GitHub" href="https://github.com/Gomasy" />
-            <Account icon="&#xf4f6;" text="Mastodon" href="https://don.gomasy.jp/@gomasy" />
-            <Account icon="&#xf099;" text="Twitter (local only)" href="https://twitter.com/u597" />
+            <Account icon="&#xf17e;" text="Skype" href="skype:ktomohiro365" />
             <Account icon="&#xf1b6;" text="Steam" href="https://steamcommunity.com/id/gomasy" />
+            <Account icon="&#xf392;" text="Discord" href="javascript:alert('Gomasy#2565');" />
+            <Account icon="&#xf4f6;" text="Mastodon (don.gomasy.jp)" href="https://don.gomasy.jp/@gomasy" />
+            <Account icon="&#xf099;" text="Twitter (local only)" href="https://twitter.com/u597" />
         </div>
     </div>
 
--- src/components/services.js
+++ src/components/services.js
@@ -5,6 +5,7 @@ export const Services = () =>
     <div class="services">
         <Header text="Services" />
         <div class="services-container">
+            <Service icon="&#xf781;" text="Blog" href="https://blog.gomasy.jp/" />
             <Service icon="&#xf4f6;" text="Mastodon Server (don.gomasy.jp)" href="https://don.gomasy.jp/about/more" />
             <Service icon="&#xf187;" text="Arch Linux Repository" href="https://repos.gomasy.jp/os/x86_64/" />
         </div>
