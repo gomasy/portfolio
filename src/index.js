0187d614a09b63734572a55af0afb76b1919fb4a
Gomasy <nyan@gomasy.jp>
2020-03-07 04:59:43 +0900

    object-assign only loads when doesn't support Object.assign

--- src/index.js
+++ src/index.js
@@ -7,7 +7,9 @@ import './contents/style.scss';
 import './contents/favicon.ico';
 
 // Object.assign polyfill
-Object.assign = require('object-assign');
+if (typeof Object.assign !== 'function') {
+    Object.assign = require('object-assign');
+}
 
 // google analytics
 const ga = new gaTrack('UA-54951000-1');
