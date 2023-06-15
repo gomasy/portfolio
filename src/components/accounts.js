d26b336656c1a1917f931ea51e7cc7b7bdefc953
Gomasy <nyan@gomasy.jp>
2023-06-15 11:38:22 +0900

    Update discord link

--- src/components/accounts.js
+++ src/components/accounts.js
@@ -8,7 +8,7 @@ export const Accounts = () =>
             <Account icon="&#xf09b;" text="GitHub" href="https://github.com/Gomasy" />
             <Account icon="&#xf17e;" text="Skype" href="skype:ktomohiro365" />
             <Account icon="&#xf1b6;" text="Steam" href="https://steamcommunity.com/id/gomasy" />
-            <Account icon="&#xf392;" text="Discord" href="javascript:alert('@gomasy');" />
+            <Account icon="&#xf392;" text="Discord" href="https://lookup.guru/378157085524885506" />
             <Account icon="&#xf4f6;" text="Mastodon (don.gomasy.jp)" href="https://don.gomasy.jp/@gomasy" />
             <Account icon="&#xf099;" text="Twitter (local only)" href="https://twitter.com/u597" />
         </div>
